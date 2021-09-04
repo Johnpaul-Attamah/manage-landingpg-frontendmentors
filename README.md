0e455badb9f44593f14e7119cd0df4015d55e307
johnpaul-attamah <bohrplus@gmail.com>
2021-09-04 21:56:23 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,140 @@
+# Frontend Mentor - Manage landing page solution
+
+This is a solution to the [Manage landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/manage-landing-page-SLXqC6P5). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See all testimonials in a horizontal slider
+- Receive an error message when the newsletter sign up `form` is submitted if:
+  - The `input` field is empty
+  - The email address is not formatted correctly
+
+### Screenshot
+
+### Links
+
+- Solution URL: [view codes](https://www.frontendmentor.io/solutions/responsive-room-homepage-using-css-flexbox-and-vanilla-javascript-kB11NzD5N)
+- Live Site URL: [Go live](https://vigorous-noether-686b27.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Flexbox
+- Mobile-first workflow
+- vanilla Javascript
+
+### What I learned
+
+## HTML 
+- How to structure markup for pages
+- How to structure for mobile first responsiveness
+- How to use responsive images with svgs
+- when to use css back ground images versus markup img tags
+- Advanced svg styling with css
+- How to change mobile and desktop structure dynamically
+- using inline svgs
+
+## Css
+- Group elements together with classes
+- Write css media query for larger screens
+- Interpret png designs into live web pages
+- Remove extra margin in images by setting display to block
+- using css transitions to control navigation display
+- how to display text or images over images
+- styling inline svgs
+- lots more
+
+```css
+...
+*::before {
+    box-sizing: border-box;
+}
+
+:root {
+    --color-white: hsl(0, 0%, 100%);
+    --color-black: hsl(0, 0%, 0%);
+    --
+...
+```
+## Javascript
+- Hiding and showing css classes
+- toggling css classes
+- setting html body to fixed when there is popup
+- implement automatic carousels with intervals
+- email validation
+
+```js
+    ...
+      const errorNotice = document.querySelector('.email-input .validation-error');
+    const submitForm = document.querySelector('.main-footer form');
+
+    function validateEmail(email) {
+        return (/^\w([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(email);
+    }
+
+
+    function submitSuccess(message) {
+        let div = document.createElement('div');
+        div.setAttribute('style', `
+            position: fixed;
+            text-align: center;
+            font-size: 2rem;
+            bottom: 40%;
+       
+    ...
+
+
+    ...
+         index = 0;
+        } else {
+            index++;
+        }
+        
+        moveToSlide(activeSlide, activeIndicator, slides[index], indicators[index]);
+    }
+
+    indicatorBox.addEventListener('click', (e) => {
+        if(!e.target.closest('.slider-controls div')) return;
+        targetIndex = indicators.findIndex(indicator => e.target === indicator);
+        index = targetIndex;
+
+        let targetIndicator = indicators[targetIndex];
+        let targetSlide = slides[targetIndex];
+    ...
+```
+### Continued development
+
+- build more pages with css flexbox
+- structure more advanced templates / layouts
+- responsive design
+- advanced dom manipulations
+- basic Javascript aapplications
+
+
+## Author
+
+- Website - [Johnpaul Attamah](https://github.com/Johnpaul-Attamah)
+- Frontend Mentor - [Johnpaul-Attamah](https://www.frontendmentor.io/profile/Johnpaul-Attamah)
+- LinkedIn - [Johnpaul](https://www.linkedin.com/in/johnpaul-attamah-4b265983/)
+
---
+++ app.js
@@ -0,0 +1,160 @@
+(() => {
+    /*******************
+     * Toggle Navigation
+     *******************/
+
+    const nav = document.querySelector('.heading .navigation'),
+        overlay = document.querySelector('.overlay'),
+        toggleContainer = document.querySelector('.heading .navigation-toggle'),
+        toggleIcon  = document.querySelector('.heading .navigation-toggle img');
+        mainHeader= document.querySelector('.heading-intro');
+
+    toggleContainer.addEventListener('click', () => {
+        if(nav.classList.contains('active')) {
+            nav.classList.remove('active');
+            overlay.classList.remove('active');
+            toggleIcon.src = './images/vectors/icons/icon-hamburger.svg';
+
+            //Restore body
+            mainHeader.classList.remove('active');
+            const scrollY = document.body.style.top;
+            document.body.style.position = '';
+            document.body.style.top = '';
+            window.scrollTo(0, parseInt(scrollY || '0') * -1);
+            toggleContainer.classList.remove('close');
+        } else {
+            toggleIcon.src = './images/vectors/icons/icon-close.svg';
+            nav.classList.add('active');
+            overlay.classList.add('active');
+
+            //prevent body from scrolling
+            mainHeader.classList.add('active');
+            document.body.style.position = 'fixed';
+            document.body.style.top = `-${window.scrollY}px`;
+            toggleContainer.classList.add('close');
+        }
+    })
+})();
+
+
+
+/**************************
+ * ************************
+ * Testimonials Slider*****
+ * ************************
+ **************************/
+
+(() => {
+    const slideBox = document.querySelector('.slider-container .slides-box'),
+     indicatorBox = document.querySelector('.section-slider .slider-controls');
+
+    const slides = [...slideBox.children];
+    const indicators = [...indicatorBox.children];
+
+    let index = 0;
+
+    const moveToSlide = (activeSlide, activeIndicator, targetSlide, targetIndicator) => {
+        //remove active slide from both indicators and slides
+        activeSlide.classList.remove('active');
+        activeIndicator.classList.remove('active');
+
+        //Add active class to target slides and indicators
+        targetSlide.classList.add('active');
+        targetIndicator.classList.add('active');
+    }
+
+    const moveToNextSlide = (totalSlides, index) => {
+        let activeSlide = slideBox.querySelector('.active');
+        let activeIndicator = indicatorBox.querySelector('.active');
+
+        if(index === totalSlides.length) {
+            index = 0;
+        } else {
+            index++;
+        }
+        
+        moveToSlide(activeSlide, activeIndicator, slides[index], indicators[index]);
+    }
+
+    indicatorBox.addEventListener('click', (e) => {
+        if(!e.target.closest('.slider-controls div')) return;
+        targetIndex = indicators.findIndex(indicator => e.target === indicator);
+        index = targetIndex;
+
+        let targetIndicator = indicators[targetIndex];
+        let targetSlide = slides[targetIndex];
+
+        let activeSlide = slideBox.querySelector('.active');
+        let activeIndicator = indicatorBox.querySelector('.active');
+
+        moveToSlide(activeSlide, activeIndicator, targetSlide, targetIndicator);
+
+    })
+
+    const autoSlide = setInterval(() => {
+        let activeSlide = slideBox.querySelector('.active');
+        let activeIndicator = indicatorBox.querySelector('.active');
+
+        index++
+        if(index === slides.length) index = 0;
+
+        moveToSlide(activeSlide, activeIndicator, slides[index], indicators[index]);
+    }, 8000);
+
+})();
+
+/**
+ * Subscription form validation
+ */
+
+(() => {
+    const email = document.querySelector('.main-footer input');
+    const errorNotice = document.querySelector('.email-input .validation-error');
+    const submitForm = document.querySelector('.main-footer form');
+
+    function validateEmail(email) {
+        return (/^\w([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(email);
+    }
+
+
+    function submitSuccess(message) {
+        let div = document.createElement('div');
+        div.setAttribute('style', `
+            position: fixed;
+            text-align: center;
+            font-size: 2rem;
+            bottom: 40%;
+            left: 10%;
+            z-index: 1000;
+            background-color: hsl(13, 44%, 81%);
+        `);
+        div.innerHTML = message;
+
+        setTimeout(() => {
+            div.parentNode.removeChild(div)
+        }, 5000);
+
+        document.body.appendChild(div)
+    }
+
+
+    submitForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        if(validateEmail(email.value)) {
+            submitSuccess('...Subscribed thank you...');
+            email.classList.remove('error');
+            errorNotice.innerHTML = '';
+            email.value = '';
+        } else if(email.value === '') {
+            errorNotice.innerHTML = `The 'input' field is empty`;
+            email.classList.add('error');
+        } else {
+            errorNotice.innerHTML = 'please enter a valid email';
+            email.classList.add('error');
+        }
+    })
+
+
+})();
+    
